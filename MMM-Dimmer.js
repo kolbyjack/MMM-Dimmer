7573b17865479b35dc94c3ca4020fd33281911e7
Jon Kolb <kolbyjack@gmail.com>
2020-04-19 19:59:42 -0400

    Use calculations from https://www.esrl.noaa.gov/gmd/grad/solcalc/calcdetails.html

--- MMM-Dimmer.js
+++ MMM-Dimmer.js
@@ -1,47 +1,26 @@
 // MMM-Dimmer.js
 
-function daysIntoYear() {
-  var date = new Date();
-  var nowUTC = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
-  var yearStartUTC = Date.UTC(date.getFullYear(), 0, 0);
+function MOD(a, b) { return a % b; }
+function RADIANS(degrees) { return degrees * Math.PI / 180; }
+function DEGREES(radians) { return radians * 180 / Math.PI; }
+function SIN(radians) { return Math.sin(radians); }
+function ASIN(sin) { return Math.asin(sin); }
+function COS(radians) { return Math.cos(radians); }
+function ACOS(cos) { return Math.acos(cos); }
+function TAN(radians) { return Math.tan(radians); }
+function ATAN(tan) { return Math.atan(tan); }
+function ATAN2(x, y) { return Math.atan2(y, x); }
 
-  return (nowUTC - yearStartUTC) / 24 / 60 / 60 / 1000;
-}
+function setSunTime(d, t) {
+  var result = new Date();
+  var h = t * 24;
+  var m = (h % 1) * 60;
+  var s = (m % 1) * 60;
 
-function deg2rad(degrees) {
-  return degrees * Math.PI / 180;
-}
+  result.setTime(d.getTime());
+  result.setUTCHours(Math.floor(h), Math.floor(m), Math.floor(s));
 
-function rad2deg(radians) {
-  return radians * 180 / Math.PI;
-}
-
-function normalize(degrees) {
-  return (degrees + 1080) % 360;
-}
-
-function sindeg(degrees) {
-  return Math.sin(deg2rad(degrees));
-}
-
-function asindeg(sin) {
-  return rad2deg(Math.asin(sin));
-}
-
-function cosdeg(degrees) {
-  return Math.cos(deg2rad(degrees));
-}
-
-function acosdeg(cos) {
-  return rad2deg(Math.acos(cos));
-}
-
-function tandeg(degrees) {
-  return Math.tan(deg2rad(degrees));
-}
-
-function atandeg(tan) {
-  return rad2deg(Math.atan(tan));
+  return result;
 }
 
 Module.register("MMM-Dimmer", {
@@ -50,17 +29,17 @@ Module.register("MMM-Dimmer", {
     longitude: -81.5812,
     latitude: 28.419411,
     maxDim: 0.9,
-    minutesToFullyDim: 15,
+    transitionDuration: 15 * 60 * 1000,
+    updateInterval: 30 * 1000,
   },
 
   start: function() {
     var self = this;
 
-    var sunrise = self.getSunTime("sunrise");
-    var sunset = self.getSunTime("sunset");
-    console.log(`day length=${sunset - sunrise}`);
+    self.now = new Date();
+    self.times = self.getSunTimes(new Date());
 
-    //setInterval(function() { self.getData(); }, self.config.updateInterval);
+    setInterval(function() { self.updateDom(); }, self.config.updateInterval);
   },
 
   notificationReceived: function(notification, payload, sender) {
@@ -71,77 +50,86 @@ Module.register("MMM-Dimmer", {
     // Do nothing
   },
 
-  getSunTime: function(which) {
-    // From https://www.edwilliams.org/sunrise_sunset_algorithm.htm
-    var zeniths = {
-      "official": 90 + 50 / 60,
-      "civil": 96,
-      "nautical": 102,
-      "astronomical": 108,
-    };
-    var zenith = zeniths[this.config.zenithType] || zeniths["official"];
-    var latitude = this.config.latitude;
-    var N = daysIntoYear();
-    var lngHour = this.config.longitude / 15;
-    var t = N + ((which === "sunrise" ? 6 : 18) - lngHour) / 24;
-    var M = (0.9856 * t) - 3.289;
-    var L = normalize(M + (1.916 * sindeg(M)) + (0.020 * sindeg(2 * M)) + 282.634);
-    var RA = normalize(atandeg(0.91764 * tandeg(L)));
-    var Lquadrant = Math.floor(L / 90) * 90;
-    var RAquadrant = Math.floor(RA / 90) * 90;
-    RA = (RA + Lquadrant - RAquadrant) / 15;
-    var sinDec = 0.39782 * sindeg(L);
-    var cosDec = cosdeg(asindeg(sinDec));
-    var cosH = (cosdeg(zenith) - (sinDec * sindeg(latitude))) / (cosDec * cosdeg(latitude));
-    var H = acosdeg(cosH);
-    if (which === "sunrise") {
-      if (cosH > 1) {
-        return NaN;
-      }
-      H = 360 - H;
-    } else {
-      if (cosH < -1) {
-        return NaN;
-      }
-    }
-    H /= 15;
-    var T = H + RA - (0.06571 * t) - 6.622;
-    var UT = (T + 48 + 1) % 24;
-
-    //*
-    console.log("=============================================================");
-    console.log(`Calculating ${which}`);
-    console.log(`zenith=${zenith}`);
-    console.log(`latitude=${latitude}`);
-    console.log(`N=${N}`);
-    console.log(`t=${t}`);
-    console.log(`M=${M}`);
-    console.log(`L=${L}`);
-    console.log(`RA=${RA}`);
-    console.log(`Lquadrant=${Lquadrant}`);
-    console.log(`RAquadrant=${RAquadrant}`);
-    console.log(`sinDec=${sinDec}`);
-    console.log(`cosDec=${cosDec}`);
-    console.log(`cosH=${cosH}`);
-    console.log(`H=${H}`);
-    console.log(`T=${T}`);
-    console.log("=============================================================");
-    // */
-    console.log(`${which}=${Math.floor(UT)}:${Math.floor((UT - Math.floor(UT)) * 60)}`);
-
-    return UT;
-  },
-
   getDom: function() {
     var self = this;
-    var wrapper = document.createElement("div");
+    var now = new Date();
+    var active = true;
+    var opacity = self.config.maxDim;
+    var sunrise = self.times.sunrise.getTime();
+    var startToBrighten = sunrise - self.config.transitionDuration;
+    var sunset = self.times.sunset.getTime();
+    var finishDimming = sunset + self.config.transitionDuration;
 
-		// Create overlay
-		//var overlay = document.createElement("div");
-		//overlay.id = "overlay";
-		//overlay.innerHTML += "<div class=\"black_overlay\"></div>";
-		//document.body.insertBefore(overlay, document.body.firstChild);
+    if (now.getTime() < startToBrighten) {
+      // Do nothing
+    } else if (now.getTime() < sunrise) {
+      opacity = self.config.maxDim * (sunrise - now.getTime()) / self.config.transitionDuration;
+    } else if (now.getTime() < sunset) {
+      active = false;
+    } else if (now.getTime() < finishDimming) {
+      opacity = self.config.maxDim * (now.getTime() - sunset) / self.config.transitionDuration;
+    } else {
+      self.times = self.getSunTimes(now);
+    }
 
-    return wrapper;
+    var overlay = document.getElementById("MMM-Dimmer-overlay");
+    if (overlay === null) {
+      if (active) {
+        overlay = document.createElement("div");
+        overlay.id = "MMM-Dimmer-overlay";
+        overlay.style.background = "#000";
+        overlay.style.position = "absolute";
+        overlay.style.top = "-60px";
+        overlay.style.left = "-60px";
+        overlay.style.right = "-60px";
+        overlay.style.bottom = "-60px";
+        overlay.style["z-index"] = 9999;
+        document.body.insertBefore(overlay, document.body.firstChild);
+      }
+    } else if (!active) {
+      overlay.parentNode.removeChild(overlay);
+    }
+
+    if (active) {
+      overlay.style.opacity = opacity;
+    }
+
+    return "";
+  },
+
+  getSunTimes: function(d) {
+    // From https://www.esrl.noaa.gov/gmd/grad/solcalc/calcdetails.html
+    // TODO: Handle cases where the sun never rises or never sets
+    var B3 = this.config.latitude;
+    var B4 = this.config.longitude;
+    var B5 = 0; // UTC
+    var F2 = d.getTime() / 86400000 + 2440587.5;
+    var G2 = (F2 - 2451545) / 36525;
+    var I2 = MOD(280.46646 + G2 * (36000.76983 + G2 * 0.0003032), 360);
+    var J2 = 357.52911 + G2 * (35999.05029 - 0.0001537 * G2);
+    var K2 = 0.016708634 - G2 * (0.000042037 + 0.0000001267 * G2);
+    var L2 = SIN(RADIANS(J2)) * (1.914602 - G2 * (0.004817 + 0.000014 * G2)) + SIN(RADIANS(2 * J2)) * (0.019993 - 0.000101 * G2) + SIN(RADIANS(3 * J2)) * 0.000289;
+    var M2 = I2 + L2;
+    var N2 = J2 + L2;
+    var O2 = (1.000001018 * (1 - K2 * K2)) / (1 + K2 * COS(RADIANS(N2)));
+    var P2 = M2 - 0.00569 - 0.00478 * SIN(RADIANS(125.04 - 1934.136 * G2));
+    var Q2 = 23 + (26 + ((21.448 - G2 * (46.815 + G2 * (0.00059 - G2 * 0.001813)))) / 60) / 60;
+    var R2 = Q2 + 0.00256 * COS(RADIANS(125.04 - 1934.136 * G2));
+    var S2 = DEGREES(ATAN2(COS(RADIANS(P2)), COS(RADIANS(R2)) * SIN(RADIANS(P2))));
+    var T2 = DEGREES(ASIN(SIN(RADIANS(R2)) * SIN(RADIANS(P2))));
+    var U2 = TAN(RADIANS(R2 / 2)) * TAN(RADIANS(R2 / 2));
+    var V2 = 4 * DEGREES(U2 * SIN(2 * RADIANS(I2)) - 2 * K2 * SIN(RADIANS(J2)) + 4 * K2 * U2 * SIN(RADIANS(J2)) * COS(2 * RADIANS(I2)) - 0.5 * U2 * U2 * SIN(4 * RADIANS(I2)) - 1.25 * K2 * K2 * SIN(2 * RADIANS(J2)));
+    var W2 = DEGREES(ACOS(COS(RADIANS(90.833)) / (COS(RADIANS(B3)) * COS(RADIANS(T2))) - TAN(RADIANS(B3)) * TAN(RADIANS(T2))));
+    var X2 = (720 - 4 * B4 - V2 + B5 * 60) / 1440;
+    var Y2 = X2 - W2 * 4 / 1440;
+    var Z2 = X2 + W2 * 4 / 1440;
+
+    var sunrise = setSunTime(d, Y2);
+    var sunset = setSunTime(d, Z2);
+
+    return {
+      "sunrise": sunrise,
+      "sunset": sunset
+    }
   },
 });
